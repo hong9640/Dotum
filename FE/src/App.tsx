8ca93bd2174f9445797a6679c93258efbc6fafa3
oesara <osrosr888@naver.com>
2025-10-16 17:53:20 +0900

    init: FE 생성(shadcn 제외)

---
+++ FE/src/App.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="w-full text-center">
+        <h1 className="text-3xl font-sans font-bold text-blue-600">
+          Pretendard 적용 완료 🎉
+        </h1>
+        <p className="text-gray-500 mt-2">
+          Tailwind 3.4.17 + Pretendard 폰트 세팅 성공!
+        </p>
+        <div className="flex justify-center items-center">
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <div className="flex justify-center items-center">
+          <div className="flex items-center justify-center w-40 h-12 bg-green-900 rounded-lg">
+            <p className="font-bold text-lg !text-white">Hello</p>
+          </div>
+        </div>
+      </div>
+      {/* <h1>Vite + React</h1> */}
+      {/* <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+
+    </>
+
+  )
+}
+
+export default App
