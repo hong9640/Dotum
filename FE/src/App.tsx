41bae1267b5fe5ed2b0511c84bb7440a295ada5f
oesara <osrosr888@naver.com>
2025-10-24 15:01:17 +0900

    fix: 홈페이지 코드 수정, 이미지 추가, 버튼 컴포넌트 연결

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -8,6 +8,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
+      {/* <div className="min-h-screen bg-white"> */}
         <NavigationBar />
         <main>
           <Routes>
